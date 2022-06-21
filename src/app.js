3ff0753f55afb840c9d726ce6909f273f45d18a7
Jhoseb <jhoseb29@gmail.com>
2022-06-21 18:21:25 -0500

    Iniciando proyecto

---
+++ src/app.js
@@ -0,0 +1,35 @@
+//! Dependencias
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+//? Import files
+const config = require('./config')
+
+//! Configuracion inicial
+const app = express()
+
+//? Habilitar la informacion entrante en formato Json
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(cors())
+
+if(config.nodeEnv === 'development'){
+    app.use(morgan("dev"))
+} else {
+    app.use(morgan("combined"))
+}
+
+//*Routes
+app.use("/api/v1/users")
+app.use("/api/v1/auth")
+
+app.listen(config.port, ()=>{
+    console.log(`Server started at port ${config.port}`)
+})
+
+module.exports = {
+    app
+}
